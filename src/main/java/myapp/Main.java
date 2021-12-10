b463781519b2593b35796b414e33d0ed2d8adb15
NEUROEQUILIBRIUM <rishi.mathur@neuroequilibrium.in>
2021-12-10 11:15:26 +0530

    Initial commit

--- src/main/java/myapp/Main.java
+++ src/main/java/myapp/Main.java
@@ -4,12 +4,5 @@ public class Main {
     public static void main(String args[]){
         System.out.println("Hello World-1");
         System.out.println("Hello World-2");
-        System.out.println("Hello World-2");
-        System.out.println("Hello World-2");
-        System.out.println("Hello World-2");
-        System.out.println("Hello World-2");
-        System.out.println("Hello World-2");
-        System.out.println("Hello World-2");
-        System.out.println("Hello World-2");
     }
 }
