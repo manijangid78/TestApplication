509df6bf2e437906ae69262e1033ad8aaaafaddb
NEUROEQUILIBRIUM <rishi.mathur@neuroequilibrium.in>
2021-12-10 10:32:42 +0530

    Initial commit

--- src/main/java/myapp/Main.java
+++ src/main/java/myapp/Main.java
@@ -6,9 +6,5 @@ public class Main {
         System.out.println("Hello World-2");
         System.out.println("Hello World-2");
         System.out.println("Hello World-2");
-        System.out.println("Hello World-2");
-        System.out.println("Hello World-2");
-        System.out.println("Hello World-2");
-        System.out.println("Hello World-2");
     }
 }
